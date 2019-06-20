f78e70b09bb3b2f23c11453e21614b1a0535aee6
Ethan Anderson <ethan.anderson00@gmail.com>
2019-06-20 21:25:13 +0100

    Removed unneeded parameter

--- commands/randomQuote.js
+++ commands/randomQuote.js
@@ -3,7 +3,7 @@ const { quotes } = require('../quotes.json');
 module.exports = {
 	name: 'randomquote',
 	description: 'Big shaq showers you with a free quote!',
-	execute(message, args) {
+	execute(message) {
 		let item = quotes.quotesArray[Math.floor(Math.random()*quotes.quotesArray.length)];
 		message.channel.send(item);
 	},
