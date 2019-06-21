201f6935163ded3d29ae87e148911399fb5ba377
Ethan Anderson <ethan.anderson00@gmail.com>
2019-06-21 21:27:08 +0100

    Added three new commands including first voice command

---
+++ commands/talk.js
@@ -0,0 +1,18 @@
+module.exports = {
+	name: 'talk',
+	description: "🎶 Yo Big Shaq 🎶",
+	execute(message) {
+		if (!message.guild) return;
+
+		if (message.member.voiceChannel) {
+			message.member.voiceChannel.join()
+				.then(connection => {
+					message.reply("I have successfully connected to the channel!");
+					connection.playFile('sounds/big-shaq-mans-not-hot-music-video.mp3');
+				})
+				.catch(console.log);
+		} else {
+			message.reply("You need to join a channel first");
+		}
+	},
+};
---
+++ commands/turtle.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'turtle',
+	description: '🎶 A turtle made it to the water 🎶',
+	execute(message) {
+		message.channel.send("https://www.youtube.com/watch?v=FIREcQl1EX4");
+	},
+};
